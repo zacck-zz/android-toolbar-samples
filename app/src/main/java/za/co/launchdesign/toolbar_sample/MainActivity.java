4bf1794d2b8825865829b288043b14a11276274f
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-22 19:06:42 +0200

    worked on adding action views and action providers to the toolbar

--- app/src/main/java/za/co/launchdesign/toolbar_sample/MainActivity.java
+++ app/src/main/java/za/co/launchdesign/toolbar_sample/MainActivity.java
@@ -1,7 +1,10 @@
 package za.co.launchdesign.toolbar_sample;
 
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,22 +20,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         myToolbar.setLogo(R.drawable.ic_star);
+        //get the toolbar as an action bar
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //inflarte the menu you want on the toolbar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
+
+        //configure the action
+        MenuItem searchItem = menu.findItem(R.id.action_refresh);
+        SearchView ourSearch =  (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        //lets put out a listener and act on the listerner
+        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(MainActivity.this,"Using action provider", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                //do Something when collapsed
+                return false;
+            }
+        };
+
+        //get the menu item for the action
+        MenuItemCompat.setOnActionExpandListener(searchItem, expandListener);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.action_refresh:
-                Toast.makeText(MainActivity.this,"Pressed Refresh", Toast.LENGTH_SHORT).show();
-                return true;
+            //handle action according to the item pressed
+
             case R.id.settings:
                 Toast.makeText(MainActivity.this,"Pressed Settings", Toast.LENGTH_SHORT).show();
 
