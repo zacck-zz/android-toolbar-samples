653e1f41a94d539dfdbc455d6648ff52cbfd04fa
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-22 18:44:46 +0200

    add toolbar and logo and some actions

--- app/src/main/java/za/co/launchdesign/toolbar_sample/MainActivity.java
+++ app/src/main/java/za/co/launchdesign/toolbar_sample/MainActivity.java
@@ -2,6 +2,10 @@ package za.co.launchdesign.toolbar_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setLogo(R.drawable.ic_star);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_refresh:
+                return true;
+            case R.id.settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
